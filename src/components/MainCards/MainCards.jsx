8297adcf46bac169c53ea57d1a3803c05924b5e1
Brandon SIlva <brsilva181@gmail.com>
2021-07-08 17:00:38 -0400

    main page buildout

--- src/components/MainCards/MainCards.jsx
+++ src/components/MainCards/MainCards.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './MainCards.scss'
+import chart1 from '../../asset/images/vulnerable_data.svg';
+import chart2 from '../../asset/images/Default-State.jpg'
 
 const MainCards = () => {
     return (
         <section className="mainCards">
-                <article className="mainCards__card">
-                    
+                <article className="mainCards__card1">
+                    <p>Most Vulnerable Data</p>
+                    <img className="image" src={chart1} src='chart'></img>
                 </article>
-                <article className="mainCards__card">
-
+                <article className="mainCards__card2">
+                    <img src={chart2} alt='chart' />
                 </article>
         </section>
     )
