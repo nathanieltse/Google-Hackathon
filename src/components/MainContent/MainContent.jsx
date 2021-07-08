c329fa87d5fb500534d337605abcef75fc2b4c9d
Brandon SIlva <brsilva181@gmail.com>
2021-07-08 12:11:53 -0400

    saving main content component

---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainCards from '../MainCards/MainCards'
+
+function MainContent() {
+    return (
+        <div>
+            <MainCards />
+        </div>
+    )
+}
+
+export default MainContent
