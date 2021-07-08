f661c7801bf9bf4e9cddb3547ae0dd29f8f98175
nathanieltse <nathanieltse@hotmail.com>
2021-07-08 15:10:29 -0400

    high-fi nav bar added

--- src/components/RightNav/RightNav.jsx
+++ src/components/RightNav/RightNav.jsx
@@ -1,3 +1,8 @@
+import calender from '../../asset/icon/right-side-bar-icon/calender.svg'
+import edit from '../../asset/icon/right-side-bar-icon/edit.svg'
+import plus from '../../asset/icon/right-side-bar-icon/plus.svg'
+import profile from '../../asset/icon/right-side-bar-icon/profile.svg'
+import tip from '../../asset/icon/right-side-bar-icon/tip.svg'
 import './RightNav.scss'
 
 const LeftNav =() => {
@@ -5,17 +10,21 @@ const LeftNav =() => {
         <nav>
             <ul className="right-nav">
                 <li className="right-nav__item">
-                    icon
+                    <img className="right-nav__item-icon" alt="calender icon" src={calender}/>
                 </li>
                 <li className="right-nav__item">
-                    icon
+                    <img className="right-nav__item-icon" alt="tip icon" src={tip}/>
                 </li>
                 <li className="right-nav__item">
-                    icon
+                    <img className="right-nav__item-icon" alt="edit icon" src={edit}/>
+                </li>
+                <li className="right-nav__item right-nav__item--last">
+                    <img className="right-nav__item-icon" alt="profile icon" src={profile}/>
                 </li>
                 <li className="right-nav__item">
-                    icon
+                    <img className="right-nav__item-icon " alt="plus icon" src={plus}/>
                 </li>
+
                 
             </ul>
         </nav>
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,6 +1,8 @@
 import apps from '../../asset/icon/apps.svg'
 import help from '../../asset/icon/help_outline.svg'
 import settings from '../../asset/icon/settings.svg'
+import google from '../../asset/icon/google.svg'
+import avatar from '../../asset/icon/avatar.svg'
 import './TopNav.scss'
 
 
@@ -9,13 +11,14 @@ const TopNav = () => {
     return (
         <nav className="top-nav">
             <div className="top-nav__left">
-                <p className="top-nav__logo">logo</p>
-                <p className="top-nav__name">logo Name</p>
+                <img src={google} className="top-nav__logo" alt="google icon"/>
+                <h2 className="top-nav__page-name">Secure</h2>
             </div>
             <div className="top-nav__right">
                 <img className="top-nav__icon" src={help} alt="help icon"/>
                 <img className="top-nav__icon" src={settings} alt="setting icon"/>
                 <img className="top-nav__icon" src={apps} alt="apps icon"/>
+                <img className="top-nav__avatar" src={avatar} alt="apps icon"/>
             </div>
 
         </nav>
